117404c34ca9d81135f0eefcc3b4d791ad3daa52
PaulMGrant <Paul@paul-grant.co.uk>
2025-02-21 14:45:59 +0000

    V1.1

---
+++ paul.js
@@ -0,0 +1,60 @@
+//Paul's code 2025
+
+/* multi
+line
+commented
+out */
+
+// let javascriptIsFun = true;
+// console.log(javascriptIsFun);
+
+// console.log(typeof true);
+// console.log(typeof javascriptIsFun);
+// console.log(typeof 23);
+// console.log(typeof 'Paolo');
+
+// javascriptIsFun = 'YES';
+// console.log(typeof javascriptIsFun);
+
+// let year;
+// console.log(year);
+// console.log(typeof year);
+
+// year = 2025;
+// console.log(year);
+// console.log(typeof year);
+
+// console.log(typeof null);
+
+// let age = 30;
+// console.log(age);
+// age = age+1;
+// console.log(age);
+
+// // const birthYear = 1978; //const cannot be changed or declared empty
+// // birthYear = 1990;
+
+// var job = 'programmer';
+// job = 'teacher';
+
+// const birthYearPaul = 1978;
+// const year = 2025;
+// const agePaul = year - birthYearPaul;
+// console.log(agePaul);
+// const frenchRule = ((agePaul/2)+7);
+// console.log(frenchRule);
+
+// // const firstName = 'Paul';
+// // const lastName = 'Grant';
+// // console.log(firstName + ' ' + lastName);
+
+// //Assignment opperators
+// // let x = 10+5; //15
+// // x+=10; // x = x + 10 = 25
+// // x*=4; //x = x * 4 = 100
+// // x++; // x = x + 1 = 101
+// // console.log(x);
+
+// //comparison operators
+// console.log(agePaul > 50); // >, <, >=, <=
+// console.log(agePaul >= 47);
